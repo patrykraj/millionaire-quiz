88dba2143124517f0cb53e3695bfaeeffcb1e193
Patryk Krajewski <patrykraj@interia.pl>
2020-03-29 16:53:27 +0200

    fix#1

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -13,11 +13,11 @@ import "../styles/Quiz.css";
 
 const Quiz = () => {
   return (
-    <BrowserRouter basename="/millionaire-quiz">
+    <BrowserRouter basename={"/millionaire-quiz"}>
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/game" exact component={Game} />
+          <Route path="/game" component={Game} />
           <Route path="/ranking" component={Ranking} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />

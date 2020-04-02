40971d991a072355ee58e3de028239ee1ff0a410
Patryk Krajewski <patrykraj@interia.pl>
2020-04-02 23:37:17 +0200

    added route transition

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import Home from "./Home";
 import Game from "./Game";
@@ -8,23 +9,42 @@ import Ranking from "./Ranking";
 import About from "./About";
 import Contact from "./Contact";
 import Error from "./Error";
-
 import "../styles/Quiz.css";
 
 const Quiz = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+  window.addEventListener("orientationchange", function() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/game" component={Game} />
-          <Route path="/ranking" component={Ranking} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={Error} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Router basename={process.env.PUBLIC_URL}>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={450} classNames="fade">
+                <Switch location={location}>
+                  <Route path="/" exact component={Home} />
+                  <Route path="/game" component={Game} />
+                  <Route path="/ranking" component={Ranking} />
+                  <Route path="/about" component={About} />
+                  <Route path="/contact" component={Contact} />
+                  <Route component={Error} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
+      </Router>
+    </div>
   );
 };
 

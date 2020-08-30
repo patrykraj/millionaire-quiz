2d9ca55f44eaefee18837dec8fe0ab26ed19d0c3
Patryk Krajewski <patrykraj@interia.pl>
2020-08-30 15:54:49 +0200

    Installed react-icons & updated contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 import "../styles/Contact.css";
 
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
+
 const Contact = () => {
   return (
     <div className="contact--container page-container">
-      <h2 className="contact-header">Contact me!</h2>
-      <form className="contact-form">
-        <textarea className="message" placeholder="Your message..."></textarea>
-        <input
-          className="contact-email"
-          type="email"
-          placeholder="Your email"
-        ></input>
-        <button className="submit--message__btn main--btn">Send</button>
-      </form>
+      <h2 className="contact-header">Contact me and check out my work:</h2>
+      <div className="contact-links-container">
+        <li>
+          <a
+            href="https://github.com/patrykraj"
+            className="contact-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>
+              <AiFillGithub />
+            </span>
+            patrykraj
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://linkedin.com/in/patryk-krajewski-9997071b4"
+            className="contact-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>
+              <AiFillLinkedin />
+            </span>
+            Patryk Krajewski
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:patrykraj@interia.pl"
+            className="contact-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>
+              <AiOutlineMail />
+            </span>
+            patrykraj@interia.pl
+          </a>
+        </li>
+      </div>
     </div>
   );
 };
